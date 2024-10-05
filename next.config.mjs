33328758d2ca1dc3272176c0375486b6c5f8bd1a
Rohit <rohitchowdhary20122002@gmail.com>
2024-10-06 00:23:40 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["cdn.imagin.studio"]
+    }
+};
 
 export default nextConfig;
+
+
+
+
+
+// const headers = {
+//     'x-rapidapi-key': '43e69d4906msh65041ea646503c4p1f94f1jsna4cbac30a963',
+//     'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+// }
+
+
+
+
